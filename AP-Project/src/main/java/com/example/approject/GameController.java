748cbe940bf195a6be5f7005be119fc42125e0ae
Aditya A Deshpande <89138194+Aditya-A-Deshpande@users.noreply.github.com>
2021-12-19 16:41:01 +0530

    Modified Game screen commit #1

--- AP-Project/src/main/java/com/example/approject/GameController.java
+++ AP-Project/src/main/java/com/example/approject/GameController.java
@@ -1,7 +1,17 @@
 package com.example.approject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 public class GameController {
 
@@ -25,6 +35,28 @@ public class GameController {
 
     @FXML
     private ImageView player2;
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Parent root;
+
+    @FXML
+    void onClickReturn(ActionEvent event) {
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
